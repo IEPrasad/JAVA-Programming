83c9d18ce6e0ad01cdc903c57d40e74d0a801bec
IEPrasad <126505521+IEPrasad@users.noreply.github.com>
2024-02-04 03:00:26 +0530

    Add files via upload

---
+++ first_java_file.java
@@ -0,0 +1,7 @@
+public class first_java_file {
+  public static void main(String[] args) throws Exception {
+    System.out.println("Hello, World!");
+    System.out.println("This is my first java code");
+    System.out.println("This is going to be my favorit");
+  }
+}
