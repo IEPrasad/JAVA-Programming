33e55aa966937a75a5eb0a2aacbd8e716c7397ef
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-08-29 23:54:02 +0530

    Update add-two-numbers.java

--- add-two-numbers.java
+++ add-two-numbers.java
@@ -8,3 +8,41 @@ public class AddTwoNumbers {
 }
 
 //  This is how to add two numbers with Java Programming.
+
+
+// Improvement of the above code 
+
+public class main AddTwoNUmbers {
+  public static void main(String[] args) {
+    int num1 = 5;
+    int num2 = 10;
+    int sum = num1 + num2;
+    System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
