a187fba190a147ac38a8bbe39ccc87d51e2747c8
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-08-29 23:56:45 +0530

    Update Multiply-two-numbers.java

--- Multiply-two-numbers.java
+++ Multiply-two-numbers.java
@@ -1,9 +1,19 @@
-public class MultiPlyTwoNumber {
+public class MultiPlyTwoNumbers {
   public static void main(String[] args) {
     int num1 = 4;
     int num2 = 7;
     int product = num1 * num2;
     System.out.println("The product is: " + product);
   }
+}
+
+//  Improvement of abouve code
+
+public class MultiplyTwoNumbers {
+  public static void main(String[] args) {
+    int num1 = 4;
+    int num2 = 7;
+    int product = num1 * num2;
+    System.out.println("The product of " + num1 + " and " + num2 " is: " + product);  }
 }
 
