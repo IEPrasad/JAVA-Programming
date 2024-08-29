b85f3dadaacedb8a1f0ca42d7fd89a6a96dc836c
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-08-29 23:46:54 +0530

    Create Multiply-two-numbers.java

---
+++ Multiply-two-numbers.java
@@ -0,0 +1,9 @@
+public class MultiPlyTwoNumber {
+  public static void main(String[] args) {
+    int num1 = 4;
+    int num2 = 7;
+    int product = num1 * num2;
+    System.out.println("The product is: " + product);
+  }
+}
+
