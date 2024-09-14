14daac547a202355e6fab728f58e3f8addce8634
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-09-14 23:38:50 +0530

    Update loopsAndSwitchStatement.java

--- loopsAndSwitchStatement.java
+++ loopsAndSwitchStatement.java
@@ -10,7 +10,7 @@ public class ForLoopExample {
 }
 */
 
-
+/*
 public class WhileLoopExample {
     public static void main(String[] args) {
         int i = 1;
@@ -21,3 +21,17 @@ public class WhileLoopExample {
         }
     }
 }
+
+*/
+
+public class DoWhileLoopExample {
+    public static void main(String[] args) {
+        int i = 1;
+        // Do-while loop to print numbers from 1 to 5
+        do {
+            System.out.println("Number: " + i);
+            i++;
+        } while (i <= 5);
+    }
+}
+
