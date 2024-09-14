96d9f46ee3500e2a7bf1583c3bddb979fc545351
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-09-14 23:38:33 +0530

    Update loopsAndSwitchStatement.java

--- loopsAndSwitchStatement.java
+++ loopsAndSwitchStatement.java
@@ -1,8 +1,23 @@
+/*
+
 public class ForLoopExample {
     public static void main(String[] args) {
         // For loop to print numbers from 1 to 5
         for (int i = 1; i <= 5; i++) {
             System.out.println("Number: " + i);
+        }
+    }
+}
+*/
+
+
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        int i = 1;
+        // While loop to print numbers from 1 to 5
+        while (i <= 5) {
+            System.out.println("Number: " + i);
+            i++;
         }
     }
 }
