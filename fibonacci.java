f28b1b120e356e6fe2d58bb0b503c0f552f83a1b
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-09-15 23:15:16 +0530

    fibonacci.java

---
+++ fibonacci.java
@@ -0,0 +1,15 @@
+public class fibonacci {
+    public static void main(Strings[] args) {
+        int a = 0;
+        int b = 1;
+        int c = 1;
+
+    System.out.println(a, b, c);
+    }
+}
+
+
+// Question:
+
+// Write a Java program to print the Fibonacci series up to n terms. The Fibonacci series starts with 0 and 1, and each subsequent term is the sum of the two preceding terms. For example, for n = 7, the Fibonacci series is:
+// 0, 1, 1, 2, 3, 5, 8
