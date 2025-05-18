7cbb8575e8adaec2a44248c0facda376b7405cb2
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2025-05-18 23:04:37 +0530

    Problem one Solution 1

---
+++ hackerrank/Solution.java
@@ -0,0 +1,36 @@
+// https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem?isFullScreen=true
+
+// import java.util.*;
+
+// public class Solution {
+
+//   public static void main(String[] args) {
+//     Scanner scan = new Scanner(System.in);
+//     String a = scan.next();
+    
+//     Scanner sc = new Scanner(System.in);
+//     int b = sc.nextInt();
+
+//     System.out.println("myString is: " + a);
+//     System.out.println("myInt is: " + b);
+//   }
+// }
+
+
+
+import java.util.*;
+
+public class Solution {
+
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+    int d = sc.nextInt();
+
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+  }
+}
